9921d4205bc09bfd00146075a53335b968d9d91a
jaey-oung <jaeywodud7@gmail.com>
2025-04-28 20:39:13 +0900

    feat: Spring Boot 부트 배너 설정 및 서버 포트 설정 추가
    spring.main.banner-mode, spring.banner.location, spring.banner.image.location 설정을 추가하여
    부트 시 배너 출력 방식을 관리할 수 있도록 구성
    server.port 설정을 통해 기본 포트 번호를 변경하고,
    0 설정 시 랜덤 포트로 할당하는 기능 추가

---
+++ src/main/java/com/example/springbootquickstartstudy/banner/DisableBannerApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootquickstartstudy.banner;
+
+import org.springframework.boot.Banner;
+import org.springframework.boot.SpringApplication;
+
+public class DisableBannerApplication {
+    public static void main(String[] args) {
+        // 배너를 출력하지 않고 실행
+        SpringApplication application = new SpringApplication(DisableBannerApplication.class);
+        application.setBannerMode(Banner.Mode.OFF);
+        application.run(args);
+    }
+}
--- src/main/java/com/example/springbootquickstartstudy/webapplicationtype/ReactiveTypeApplication.java
+++ src/main/java/com/example/springbootquickstartstudy/webapplicationtype/ReactiveTypeApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ReactiveTypeApplication {
     public static void main(String[] args) {
-        // 스프링 WebFlux 기반 리액티브 웹 서버로 실행
+        // 스프링 WebFlux 기반 비동기/논블로킹 리액티브 웹 서버로 실행
         SpringApplication application = new SpringApplication(ReactiveTypeApplication.class);
         application.setWebApplicationType(WebApplicationType.REACTIVE);
         application.run(args);
