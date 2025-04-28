538eb197c21c9322e447fdd695ebf232f3851a9e
jaey-oung <jaeywodud7@gmail.com>
2025-04-28 13:00:31 +0900

    feat: Spring Boot 스타터 프로젝트 기본 구조 추가
    Spring Boot 3.4.5 기반으로
    초기 디렉토리 구조,
    Gradle 빌드 파일,
    기본 설정 파일을 추가

---
+++ src/main/java/com/example/springboot_quickstart_study/SpringbootQuickstartStudyApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboot_quickstart_study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootQuickstartStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootQuickstartStudyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springboot_quickstart_study/SpringbootQuickstartStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboot_quickstart_study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootQuickstartStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
