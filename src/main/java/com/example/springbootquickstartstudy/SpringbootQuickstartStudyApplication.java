e2494a5fa5a35b8d8b41f7c6237d88831c5ac251
jaey-oung <jaeywodud7@gmail.com>
2025-07-06 22:38:32 +0900

    build: QueryDSL 경로 및 JSP ViewResolver 설정 추가

--- src/main/java/com/example/springbootquickstartstudy/SpringbootQuickstartStudyApplication.java
+++ src/main/java/com/example/springbootquickstartstudy/SpringbootQuickstartStudyApplication.java
@@ -8,16 +8,23 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 // @SpringBootApplication은 내부에 @ComponentScan을 포함하여 지정한 패키지 이하의 클래스를 자동으로 스캔하고 빈으로 등록함
 // ch3 패키지 사용 시 → @SpringBootApplication(scanBasePackages = "com.example.springbootquickstartstudy.ch3")
 // ch5 패키지 사용 시
-@SpringBootApplication(scanBasePackages = "com.example.springbootquickstartstudy.ch5")
+//@SpringBootApplication(scanBasePackages = "com.example.springbootquickstartstudy.ch5")
+//public class SpringbootQuickstartStudyApplication {
+//	public static void main(String[] args) {
+//		// SpringApplication 객체 생성
+//		SpringApplication application = new SpringApplication(SpringbootQuickstartStudyApplication.class);
+//
+//		// 웹 서버를 실행하지 않고 일반 자바 애플리케이션 모드로 설정
+//		application.setWebApplicationType(WebApplicationType.NONE);
+//
+//		// 애플리케이션 실행
+//		application.run(args);
+//	}
+//}
+
+@SpringBootApplication(scanBasePackages = "com.example.springbootquickstartstudy.ch6")
 public class SpringbootQuickstartStudyApplication {
 	public static void main(String[] args) {
-		// SpringApplication 객체 생성
-		SpringApplication application = new SpringApplication(SpringbootQuickstartStudyApplication.class);
-
-		// 웹 서버를 실행하지 않고 일반 자바 애플리케이션 모드로 설정
-		application.setWebApplicationType(WebApplicationType.NONE);
-
-		// 애플리케이션 실행
-		application.run(args);
+		SpringApplication.run(SpringbootQuickstartStudyApplication.class, args);
 	}
 }
